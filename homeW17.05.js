d2aaa5860336c50f70007abf7ee1a260c0f836fb
YuliiaT <za1605@ukr.net>
2023-05-18 01:43:00 +0300

    second commit

--- homeW17.05.js
+++ homeW17.05.js
@@ -105,3 +105,18 @@ let nums2 = [21,11,3];
 nums1.sort(function (a, b){
     return b-a;});
 console.log(nums2);
+
+- є масив
+//let coursesAndDurationArray = [
+   // {title: 'JavaScript Complex', monthDuration: 5},
+   // {title: 'Java Complex', monthDuration: 6},
+    //{title: 'Python Complex', monthDuration: 6},
+    //{title: 'QA Complex', monthDuration: 4},
+   // {title: 'FullStack', monthDuration: 7},
+   // {title: 'Frontend', monthDuration: 4}
+//відсортувати його за спаданням за monthDuration
+// відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+ //за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+
+
+    let
