8d06dc167a293c8ffbcf517b29db32ff2d59111f
YuliiaT <za1605@ukr.net>
2023-05-20 15:09:44 +0300

    five commit

--- homeW17.05.js
+++ homeW17.05.js
@@ -138,5 +138,98 @@ console.log(sort2);
 
 let sort3 = coursesAndDurationArray.map((item, index)=>
 
-    ({...item, id:index +10}));
+    ({...item, id:index +1}));
 console.log(sort3);
+
+//описати колоду карт (від 6 до туза без джокерів)
+//знайти піковий туз
+// всі шістки
+//всі червоні карти всі буби всі трефи від 9 та більше
+
+//{
+    //cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+       // value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+   // color:'', // 'red','black'
+
+let cards = [
+    { cardSuit: 'spade', value: '6', color: 'black' },
+    { cardSuit: 'diamond', value: '6', color: 'red' },
+    { cardSuit: 'heart', value: '6', color: 'red' },
+    { cardSuit: 'clubs', value: '6', color: 'black' },
+    { cardSuit: 'spade', value: '7', color: 'black' },
+    { cardSuit: 'diamond', value: '7', color: 'red' },
+    { cardSuit: 'heart', value: '7', color: 'red' },
+    { cardSuit: 'clubs', value: '7', color: 'black' },
+    { cardSuit: 'spade', value: '8', color: 'black' },
+    { cardSuit: 'diamond', value: '8', color: 'red' },
+    { cardSuit: 'heart', value: '8', color: 'red' },
+    { cardSuit: 'clubs', value: '8', color: 'black' },
+    { cardSuit: 'spade', value: '9', color: 'black' },
+    { cardSuit: 'diamond', value: '9', color: 'red' },
+    { cardSuit: 'heart', value: '9', color: 'red' },
+    { cardSuit: 'clubs', value: '9', color: 'black' },
+    { cardSuit: 'spade', value: '10', color: 'black' },
+    { cardSuit: 'diamond', value: '10', color: 'red' },
+    { cardSuit: 'heart', value: '10', color: 'red' },
+    { cardSuit: 'clubs', value: '10', color: 'black' },
+    { cardSuit: 'spade', value: 'jack', color: 'black' },
+    { cardSuit: 'diamond', value: 'jack', color: 'red' },
+    { cardSuit: 'heart', value: 'jack', color: 'red' },
+    { cardSuit: 'clubs', value: 'jack', color: 'black' },
+    { cardSuit: 'spade', value: 'queen', color: 'black' },
+    { cardSuit: 'diamond', value: 'queen', color: 'red' },
+    { cardSuit: 'heart', value: 'queen', color: 'red' },
+    { cardSuit: 'clubs', value: 'queen', color: 'black' },
+    { cardSuit: 'spade', value: 'king', color: 'black' },
+    { cardSuit: 'diamond', value: 'king', color: 'red' },
+    { cardSuit: 'heart', value: 'king', color: 'red' },
+    { cardSuit: 'clubs', value: 'king', color: 'black' },
+    { cardSuit: 'spade', value: 'ace', color: 'black' },
+    { cardSuit: 'diamond', value: 'ace', color: 'red' },
+    { cardSuit: 'heart', value: 'ace', color: 'red' },
+    { cardSuit: 'clubs', value: 'ace', color: 'black' },
+];
+
+const card1 = cards.find((card) => card.cardSuit === `spade` && card.value === `ace`);
+console.log(card1);
+
+const card2 = cards.filter((card)=> card.value === `6`);
+console.log(card2);
+
+const card3 = cards.filter((card)=> card.color ===`red`);
+console.log(card3);
+
+const card4 = cards.filter((card)=> card.cardSuit ===`diamond`);
+console.log(card4);
+
+const card5 = cards.filter((card) =>[`9`, `10`, `ace`, `jack`, `qeen`, `kind`].includes(card.value) && card.cardSuit ===`clubs`);
+console.log(card5);
+
+
+  //  Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
+//{
+    //spades:[],
+    //    diamonds:[],
+   // hearts:[],
+    //clubs:[]}
+const card6 = cards.reduce((accumulator, cu) =>{
+    switch (cu.cardSuit){
+        case `spade`:
+            accumulator.spades.push(cu);
+            break;
+        case `diamond`:
+            accumulator.spades.push(cu);
+            break;
+        case `heart`:
+            accumulator.hearts.push(cu);
+            break;
+        case `clubs`:
+            accumulator.clubs.push(cu);
+            break;
+    }
+    return accumulator;
+},{spades:[],diamonds:[],hearts:[],clubs:[]});
+console.log(card6);
+   // взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+//написати пошук всіх об'єктів, в який в modules є sass
+//написати пошук всіх об'єктів, в який в modules є docker
